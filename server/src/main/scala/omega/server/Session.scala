c57772c92e51eb16dc8646e9f3caf8742cfee362
John Wass <wassj@ctc.com>
2022-02-09 14:37:04 -0500

    init with empty buffer when no file is used

--- server/src/main/scala/omega/server/Session.scala
+++ server/src/main/scala/omega/server/Session.scala
@@ -7,14 +7,25 @@ import omega.server.protocol._
 import java.nio.file.Path
 
 object Session {
-  def props(data: Option[Path]): Props =
-    Props(new Session(OmegaLib.newSession(data)))
+  def props(data: Option[Path]): Props = {
+    val omega = data match {
+      case None =>
+        val session = OmegaLib.newSession(None)
+        session.push(List.fill(10000)(" ").mkString)
+        session
+      case path => OmegaLib.newSession(path)
+    }
+    Props(new Session(omega))
+  }
 }
 
 class Session(session: api.Session) extends Actor {
   def receive: Receive = {
     case ConnectView(ws, view) =>
-      session.viewCb(view.offset, view.length, v => ws ! ViewUpdated(view.id, v.data()))
+      session.viewCb(view.offset, view.length, v => {
+        println(s"fired ${view.id}!")
+        ws ! ViewUpdated(view.id, v.data())
+      })
       println(s"registered view: $view")
 
     case Push(data)              => session.push(data)
